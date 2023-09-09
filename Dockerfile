ef7406632afd817b09940753b7620e0f8c6c723a
Anas Mazouni <madadj4@gmail.com>
2023-09-09 02:36:41 +0200

    ci: :construction: dockerize everything

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
-FROM node:18
+# Stage 1: Build the app
+FROM node:18 AS build
 
 # Install pnpm
 RUN npm install -g pnpm
-
-# Install ffmpeg and related dependencies
-RUN apt-get update && \
-    apt-get install -y ffmpeg libavcodec-extra && \
-    rm -rf /var/lib/apt/lists/*
 
 # Set working directory
 WORKDIR /app
@@ -14,18 +10,41 @@ WORKDIR /app
 # Copy package.json and pnpm-lock.yaml
 COPY package.json ./
 COPY pnpm-lock.yaml ./
+COPY tsconfig.json ./
+COPY src ./src
 
 # Install dependencies
-RUN pnpm install
-
-# Copy source code
-COPY . .
+RUN pnpm install --frozen-lockfile
 
 # Build the app
-RUN pnpm build
+RUN pnpm run build
+
+# Stage 2: Run the app
+FROM node:18
+
+# Install ffmpeg and related dependencies
+RUN apt-get update && \
+    apt-get install -y ffmpeg libavcodec-extra && \
+    rm -rf /var/lib/apt/lists/*
+
+# Install pnpm
+RUN npm install -g pnpm pm2
+
+# Set working directory
+WORKDIR /app
+
+COPY package.json ./
+COPY pnpm-lock.yaml ./
+COPY assets ./assets
+
+# Copy the built app from the previous stage
+COPY --from=build /app/dist .
+
+# Install production dependencies
+RUN pnpm install --frozen-lockfile --prod
 
 # Expose the port
-EXPOSE 3000
+EXPOSE 6969
 
 # Start the app
-CMD ["pnpm", "start"]
+CMD ["pm2-runtime", "node --experimental-specifier-resolution=node index.js" ]
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import ffmpeg from 'fluent-ffmpeg';
+import retry from 'p-retry';
 import { AUDIO_ENDPOINT, VIDEO_ENDPOINT } from './config/app';
 import { env } from './env';
 import { http, logger } from './logger';
@@ -33,37 +34,52 @@ const main = async () => {
     return new VideoQueue(res).next();
   });
 
-  return Promise.all([
-    app.listen(env.PORT, () => {
-      logger.info(`Server running on port http://localhost:${env.PORT}`);
-      logger.info(`> Audio stream running on http://localhost:${env.PORT}${AUDIO_ENDPOINT}`);
-      logger.info(`> Video stream running on http://localhost:${env.PORT}${VIDEO_ENDPOINT}`);
-    }),
-    new Promise((resolve, reject) =>
-      ffmpeg()
-        .input(`http://localhost:${env.PORT}${VIDEO_ENDPOINT}`)
-        .addInputOption('-stream_loop -1')
-        .input(`http://localhost:${env.PORT}${AUDIO_ENDPOINT}`) // http://hyades.shoutca.st:8043/stream
-        .addInputOption('-stream_loop -1')
-        .addOption('-map', '0:v')
-        .addOption('-map', '1:a')
-        .addInputOption('-re')
-        .videoCodec('copy')
-        .on('start', () => {
-          logger.info('Started Live Stream on: ' + env.STREAM_URL);
-        })
-        .on('error', (e) => reject(e))
-        .on('end', () => resolve('Resolved'))
-        .format('flv')
-        .output(`${env.STREAM_URL}/${env.STREAM_KEY}`, {
-          end: true
-        })
-        .run()
-    )
-  ]);
+  const server = app.listen(env.PORT, () => {
+    logger.info(`Server running on port http://localhost:${env.PORT}`);
+    logger.info(`> Audio stream running on http://localhost:${env.PORT}${AUDIO_ENDPOINT}`);
+    logger.info(`> Video stream running on http://localhost:${env.PORT}${VIDEO_ENDPOINT}`);
+  });
+
+  // Graceful shutdown
+  process.on('SIGTERM', () => {
+    logger.debug('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+      logger.debug('HTTP server closed');
+    });
+  });
+
+  return new Promise((resolve, reject) =>
+    ffmpeg()
+      .input(`http://localhost:${env.PORT}${VIDEO_ENDPOINT}`)
+      .addInputOption('-stream_loop -1')
+      .input(`http://localhost:${env.PORT}${AUDIO_ENDPOINT}`) // http://hyades.shoutca.st:8043/stream
+      .addInputOption('-stream_loop -1')
+      .addOption('-map', '0:v')
+      .addOption('-map', '1:a')
+      .addInputOption('-re')
+      .videoCodec('copy')
+      .on('start', () => {
+        logger.info('Started Live Stream on: ' + env.STREAM_URL);
+      })
+      .on('error', (e) => {
+        server.close();
+        reject(e);
+      })
+      .on('end', () => resolve('Resolved'))
+      .format('flv')
+      .output(`${env.STREAM_URL}/${env.STREAM_KEY}`, {
+        end: true
+      })
+      .run()
+  );
 };
 
-main().catch((error) => {
+retry(main, {
+  onFailedAttempt: (error) => {
+    logger.warn(`Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`);
+  },
+  retries: 4
+}).catch((error) => {
   logger.error('FATAL:' + error.message);
   process.exit(1);
 });
